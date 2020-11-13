209fc1530107a8d0579a7fa3851d2902f4ae4080
Ben Sumali <bensumali@gmail.com>
2020-11-12 22:26:19 -0800

    Added file writing

--- queueImproved/queueImproved_StreamLabsSystem.py
+++ queueImproved/queueImproved_StreamLabsSystem.py
@@ -5,7 +5,7 @@ Creator = "pac0ncrack"
 Version = "1.0.0"
 
 queue = []
-queueFile = "queueFile"
+queueFile = "queueFile.csv"
 queueOpen = False
 queueMaxCapacity = 10
 
@@ -27,15 +27,20 @@ def Execute(data):
     global queueOpen
     global queue
     command = data.GetParam(0)
+
+    # Commands that are only for mods. These commands are only available by whispering to the bot
+
     if data.IsWhisper():
         if Parent.HasPermission(data.User, "Moderator", ""):
             if command == "!openQueue":
+                # Open the queue. Will only work if the queue is not already open
                 if not queueOpen:
                     queueOpen = True
                     send_message(messageQueueOpen)
                 else:
                     send_whisper(data.User, messageQueueAlreadyOpen)
             elif command == "!closeQueue":
+                # Close the queue. Won't work if queue isn't open
                 if queueOpen:
                     queueOpen = False
                     send_message(messageQueueClosed)
@@ -44,11 +49,13 @@ def Execute(data):
 
     else:
         if queueOpen:
+            # Commands available when the queue is open
             if command == "!join":
                 userToAdd = data.User
                 if userToAdd not in queue:
                     queue.append(userToAdd)
                     send_message(userToAdd + ", you have entered the queue. Pos: #" + str(len(queue) - 1) + "!")
+                    write_queue_to_file()
                 else:
                     send_message("Dafuq, " + userToAdd + ", you already in line. You #" + str(queue.index(userToAdd)) + ".")
             elif command == "!leave":
@@ -56,6 +63,7 @@ def Execute(data):
                 if userToLeave in queue:
                     queue.remove(userToLeave)
                     send_message("Adios. " + userToLeave + " has left the queue.")
+                    write_queue_to_file()
                 else:
                     send_message(userToLeave + ", you aren't even in line. Pls.")
 
@@ -81,3 +89,10 @@ def is_queue_open():
 
 def send_whisper(user, message):
     Parent.SendStreamWhisper(user, message)
+
+def write_queue_to_file():
+    file = open(queueFile, "w")
+    for val in queue:
+        file.write(val + "\n")
+    file.close()
+    return
